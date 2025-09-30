4ff5b5e0f128424debaf4b48b8535f017ec97d8d
AnGISIta12 <angylorena0912@gmail.com>
2025-09-30 11:12:48 -0500

    lol

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.proyecto_distribuidos2530 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.proyecto_distribuidos2530 to javafx.fxml;
-    exports com.example.proyecto_distribuidos2530;
-}
