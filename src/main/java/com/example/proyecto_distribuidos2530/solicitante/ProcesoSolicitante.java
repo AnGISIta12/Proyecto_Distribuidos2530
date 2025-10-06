d4c5571ac2cd2ab36891c2ad8daa27406098818f
Violeta <violeta.fajardo@javeriana.edu.co>
2025-10-06 15:26:19 -0500

    Primera entrega completa y funcional - comunicación PS, GC y Actores

---
+++ src/main/java/com/example/proyecto_distribuidos2530/solicitante/ProcesoSolicitante.java
@@ -0,0 +1,30 @@
+package com.example.proyecto_distribuidos2530.solicitante;
+
+import org.zeromq.ZMQ;
+import java.nio.file.*;
+import java.util.List;
+
+public class ProcesoSolicitante {
+    public static void main(String[] args) throws Exception {
+        if (args.length < 1) {
+            System.out.println("Uso: java ProcesoSolicitante <archivo_peticiones>");
+            return;
+        }
+
+        String archivo = args[0];
+        ZMQ.Context context = ZMQ.context(1);
+        ZMQ.Socket requester = context.socket(ZMQ.REQ);
+        requester.connect("tcp://localhost:5555"); // puerto del GC
+
+        List<String> lineas = Files.readAllLines(Paths.get(archivo));
+        for (String linea : lineas) {
+            requester.send(linea);
+            String respuesta = requester.recvStr();
+            System.out.println("PS recibió: " + respuesta);
+            Thread.sleep(800);
+        }
+
+        requester.close();
+        context.close();
+    }
+}
